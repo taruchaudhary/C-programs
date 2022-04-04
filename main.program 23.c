f518f9f1eab1ba2b78c05a16cc00799868b39348
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-04 12:56:00 -0400

    Add files via upload

---
+++ main.program 23.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    inta=1,b=1,c=0,i;
+    printf("%d\t%d\t",a,b);
+        for(i=0;i<=10;i++)
+        {
+            c=a+b;
+            if(c<100)
+        {
+           printf("%d\t",c);
+        }
+            a=b;
+            b=c;
+
+        }
+
+    return 0;
+}
+
+
+
+
+
+
