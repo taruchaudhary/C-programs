b56e7e6a0c7f6068decf79d654fc7d304663f78d
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-04 12:11:19 -0400

    Add files via upload

---
+++ main.program 14.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    char ch;
+    printf("enter m for Monday\nt for Tuesday\nw for Wednesday\nth for Thursday\nf for Friday\ns for Saturday\nsu for Sunday");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+    case'm':
+    case'M':
+    }
+    return 0;
+}
+
+
+
+
