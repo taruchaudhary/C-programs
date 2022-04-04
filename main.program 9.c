d4f3a34831aa1fe9ff44980c51dba7633603f6f9
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-04 12:09:12 -0400

    Add files via upload

---
+++ main.program 9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int gs,bs,da,ta;
+    printf("Enter basic salary: ");
+    scanf("%d",&bs);
+    da=(10*bs)/100;
+    ta=(12*bs)/100;
+    gs=bs+da+ta;
+    printf("gross salary=%d",gs);
+
+
+
+
+
+     return 0;
+
+}
+
+
+
+
