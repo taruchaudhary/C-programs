8752a851150f7eba67a3348ffb7663dc2f0b44c1
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-06 13:43:14 -0400

    Add files via upload

---
+++ main program 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<< " Enter the value of a:"<<end1;
+    cin>>a;
+    cout<<"Enter the value of b:"<<end2;
+    cin>>b;
+    c=a+b;
+    cout<<"The sum is "<<c;
+    return 0;
+}
