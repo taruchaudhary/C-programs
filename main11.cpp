c0f635b63dfee3763cba070cc9be6c99fa84b9df
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-09 13:14:54 -0400

    Add files via upload

---
+++ main11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<=3;i++){
+        for(int j=0;j<=3;j++)
+            cout<<"i is"<<i<<"and j is"<<j<<endl;
+    }
+
+    return 0;
+}
