807c183b96d114ffcac642ea266c9eaabaa81fa5
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 13:57:31 -0400

    Add files via upload

---
+++ program using WHILE and IF statement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ;
+    cout<<"enter 0 to exit\n\n"
+    while(1)
+    {
+        cout<<"enter any value: ";
+        cin>>i;
+        cout<<"you entered: "<<i<<endl<<
+        if(i==0)
+        {
+            cout<<"program exit\n\n";
+            break;
+        }
+    }
+    return 0;
+}
