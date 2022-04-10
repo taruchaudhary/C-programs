10c04a0359c46c990daa7b8af8a2ef9f753520ed
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 13:45:17 -0400

    Add files via upload

---
+++ using INT and DOUBLE .c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int myAge;
+    int theirAge=20;
+    double myMoney=1000.24;
+    double theirMoney=367.56;
+
+    myAge;
+    cout<<myAge<<endl;
+    cout<<theirAge<<endl;
+    cout<<myMoney<<endl;
+    cout<<theirMoney<<endl;
+    cout<<endl;
+    return 0;
+}
