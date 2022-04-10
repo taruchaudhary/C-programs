6561230d19ae2291fb9c02859a4d0cfb05126274
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 13:07:47 -0400

    Add files via upload

---
+++ using FOR loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(i=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
