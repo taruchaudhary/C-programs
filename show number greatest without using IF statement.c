163d4267baed0d060f12a33eb5010be83dc78276
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 12:22:13 -0400

    Add files via upload

---
+++ show number greatest without using IF statement.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    printf("Enter the value for a:");
+    scanf("%d",&a);
+    printf("Enter the value for b: ");
+    scanf("%d",&b);
+    (a>b)?printf("a is greatest"):printf("b is greatest");
+
+
+
+
+
+
+
+     return 0;
+
+}
+
+
+
+
