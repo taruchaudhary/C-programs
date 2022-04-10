211950cc39670ba7a94d9e748e00aca46885a413
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 13:12:32 -0400

    Add files via upload

---
+++ using FOR loop hastrik increse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int(i=1;i<=3;i++);
+    {
+        for(j=3;j>=i;j--)
+            printf(" ");
+        {
+            for(k=1;k<=i*2-1;k++)
+                printf("*");
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
