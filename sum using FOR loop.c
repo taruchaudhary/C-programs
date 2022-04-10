4b67f263c59270c7d5edf6f71de6686e3a3ea307
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 12:45:17 -0400

    Add files via upload

---
+++ sum using FOR loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int i,sum=0;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d no is=%d\n",i);
+        sum=sum+i;
+    }
+    printf("sum=%d",sum);
+    return 0;
+}
+
+
+
+
+
+
