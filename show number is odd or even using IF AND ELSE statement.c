87fa0b24ca0dc9ee988ab0a616229d9e298fb795
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-10 12:25:48 -0400

    Add files via upload

---
+++ show number is odd or even using IF AND ELSE statement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int n;
+    printf("enter number: ");
+    scanf("%d",&n);
+    if(n%2==0)
+        printf("number is even");
+    else
+        printf("number is odd");
+    return 0;
+}
+
+
+
+
+
+
