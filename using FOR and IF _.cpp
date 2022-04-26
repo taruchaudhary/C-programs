23a7e78addc4ff86a1a3ff1546998043cc1209d2
Taru Malik <101206992+taruchaudhary@users.noreply.github.com>
2022-04-26 04:27:47 -0400

    Add files via upload

---
+++ using FOR and IF _.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        cout<<j;
+        cout<<"\n";
+    }
+    
+    
+    
+    
+    return 0;
+}
